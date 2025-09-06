cc42d9dc1141e9a6a0061e55c7ef8f61b46a9cfb
Mayank Dhangar <mayankdhangar78060@gmail.com>
2025-09-06 11:11:50 +0530

    All Shop Category Sections Updated - Mobiles, Audio and Charhers

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
 import { Footer } from './Components/Footer/Footer';
+import mobiles_banner from './Components/Assets/banner_mens.png'
+import audio_banner from './Components/Assets/banner_women.png'
+import chargers_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mobiles' element={<ShopCategory category="mobiles"/>}/>
-        <Route path='/audio' element={<ShopCategory category="audio"/>}/>
-        <Route path='/chargers' element={<ShopCategory category="chargers"/>}/>
+        <Route path='/mobiles' element={<ShopCategory banner={mobiles_banner} category="mobiles"/>}/>
+        <Route path='/audio' element={<ShopCategory banner={audio_banner} category="audio"/>}/>
+        <Route path='/chargers' element={<ShopCategory banner={chargers_banner} category="chargers"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=":productID" element={<Product/>}/>
         </Route>
