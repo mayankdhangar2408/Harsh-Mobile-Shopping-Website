6f4017f3b8cf56349080b3b0ff7e141e53eb23cd
Mayank Dhangar <mayankdhangar78060@gmail.com>
2025-09-07 23:01:40 +0530

    Navbar Responsiveness Added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,23 @@
-import React, {useContext, useState} from "react"
+import React, {useContext, useRef, useState} from "react"
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from  '../Assets/nav_dropdown.png'
 
 const Navbar = () => {
 
     //for changing the nav-menu red underline
     const [menu,setMenu] = useState("shop");
     const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
 
     return (
         <div className="navbar">
@@ -18,7 +25,8 @@ const Navbar = () => {
                 <img src={logo} alt="" />
                 <p>HARSH MOBILE</p>
             </div>
-            <ul className="nav-menu">
+            <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+            <ul ref={menuRef} className="nav-menu">
                 <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none', color: '#626262'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("mobiles")}}><Link style={{textDecoration: 'none', color: '#626262'}} to='/mobiles'>Mobiles</Link>{menu==="mobiles"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("audio")}}><Link style={{textDecoration: 'none', color: '#626262'}} to='/audio'>Audio</Link>{menu==="audio"?<hr/>:<></>}</li>
